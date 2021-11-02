93b01d6a17d47dd30169623374d58e001bce33ae
cook-brook <72130874+cook-brook@users.noreply.github.com>
2021-11-02 14:31:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 function setup(){
-  canvas =  createCanvas(700,600);
-  canvas.center();
-  canvas.position(400,145);
+  var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  
+  video = createCapture(VIDEO);
+  video.size(700, 900);
+  video.center();
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+  }
+  
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized');
   }
 
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+  
+      rightWristY = results[0].pose.rightWrist.y;
+      rightWristX = results[0].pose.rightWrist.x;
+      scoreRightWrist =  results[0].pose.keypoints[10].score;
+      console.log(scoreRightWrist);
+    }
+  }
+  
